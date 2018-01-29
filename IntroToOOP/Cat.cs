ff2c6b327ff20a3c60f9fdf13a45cdb51cf34292
jsales3 <jsales3>
2018-01-29 11:40:11 -0500

    Added class and attributes

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,6 +8,45 @@ namespace IntroToOOP
 {
     class Cat
     {
+        //I need STATES
 
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //properties 
+        public string Name
+        {
+            get { return this.name;}
+            set { this.name = value; }
+        }
+
+        //I need BEHAVIORS
+
+        //Constructors
+        public Cat()
+        {
+            //default constructors
+            //takes no parameters
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+
+            }
+            Console.WriteLine("Is the cat hungry?" + isHungry);
+        }
     }
 }
